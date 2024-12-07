46ddfa3e773fad7d2d91d835125fd8d694433431
RomainSav <63973220+RomainSav@users.noreply.github.com>
2024-12-07 16:49:50 +0100

    🔧 (tailwind.config.ts): Update content paths to include files from the 'lib' directory in addition to 'app' directory 🔧 (tailwind.config.ts): Refactor theme configuration for better readability and consistency 🔧 (tailwind.config.ts): Add support for the 'tailwindcss-animate' plugin 🔧 (types/src/game/index.ts): Add new enum 'StartGameResponse' to define possible game start responses

--- packages/types/src/game/index.ts
+++ packages/types/src/game/index.ts
@@ -2,3 +2,11 @@ export type Player = {
     id: string;
     name: string;
 }
+
+export enum StartGameResponse {
+    Success = "success",
+    NotEnoughPlayers = "not-enough-players",
+    GameAlreadyStarted = "game-already-started",
+    NotHost = "not-host",
+    UnexpectedError = "unexpected-error"
+}
