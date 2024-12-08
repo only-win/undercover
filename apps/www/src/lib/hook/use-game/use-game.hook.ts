958e7ff7705aa38cf08dcdb975c0a0c4b5fd6535
Gaëtan H <gaetanhus@gmail.com>
2024-12-08 16:18:15 +0100

    feat(use-game.hook.ts): extend GameState to include gameConfig, rounds, and timer for enhanced game management feat(use-game.type.ts): introduce RoundPhase and RoundState types to better represent game phases and player states

--- apps/www/src/lib/hook/use-game/use-game.hook.ts
+++ apps/www/src/lib/hook/use-game/use-game.hook.ts
@@ -8,7 +8,23 @@ let socket: Socket | null = null;
 export const useGame = (gameId: string) => {
 	const [state, setState] = useState<GameState>({
 		round: 0,
-		phase: "playing"
+		phase: "playing",
+		
+		gameConfig: {
+			votingTime: 0,
+			writingTime: 0,
+
+			players: {},
+			roles: {
+				civilian: 0,
+				uncover: 0,
+				mrWhite: 0,
+				total: 0
+			}
+		},
+
+		rounds: [],
+		timer: 0
 	});
 
 	useEffect(() => {
--- apps/www/src/lib/hook/use-game/use-game.type.ts
+++ apps/www/src/lib/hook/use-game/use-game.type.ts
@@ -1,6 +1,37 @@
-export type GamePhase = "waiting" | "playing" | "voting" | "gameover";
+export type GamePhase = "waiting" | "playing" | "gameover";
+export type RoundPhase = "writing" | "voting";
+
+export type RoundState = {
+	phase: RoundPhase;
+	activePlayer: string;
+
+	players: Record<string, {
+		word: string;
+		vote: string | null;
+	}>;
+}
 
 export type GameState = {
 	round: number;
 	phase: GamePhase;
+
+	gameConfig: {
+		votingTime: number;
+		writingTime: number;
+
+		players: Record<string, {
+			name: string;
+			role: "civilian" | "uncover" | "mrWhite";
+		}>;
+
+		roles: {
+			civilian: number;
+			uncover: number;
+			mrWhite: number;
+			total: number;
+		}
+	};
+
+	timer: number;
+	rounds: RoundState[];
 }
